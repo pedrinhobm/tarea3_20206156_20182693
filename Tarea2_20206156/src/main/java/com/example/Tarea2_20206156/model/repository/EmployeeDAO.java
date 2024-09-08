7c0cb72669228cdcebd3d63ca1c5f212f82c494f
Pedro Bustamante Melo <137945040+pedrinhobm@users.noreply.github.com>
2024-09-08 18:02:22 -0500

    Update EmployeeDAO.java

--- Tarea2_20206156/src/main/java/com/example/Tarea2_20206156/model/repository/EmployeeDAO.java
+++ Tarea2_20206156/src/main/java/com/example/Tarea2_20206156/model/repository/EmployeeDAO.java
@@ -1,8 +1,4 @@
-*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.Tarea2_20206156.model.repository;
 
 import com.example.Tarea2_20206156.model.bean.*;
